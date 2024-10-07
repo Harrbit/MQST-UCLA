9a7f4a6233a5b890acc06f56d8240809b3a5b277
Harrbit <2471691727@qq.com>
2024-10-07 00:28:37 -0700

    prelab1 done!

---
+++ physics_245/hwk_1/prb_2.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:642f51f77a70e24e2d247df7c27aed3f79ff2c300a74f042ddef4a6796ac0737
+size 1544
---
+++ qnt_sci_410/pre_lab/prelab_1/prob_1.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4062f8289ef2af845d10cdc83448889722dce97848ed8b1d38db918e10c3b145
+size 606
