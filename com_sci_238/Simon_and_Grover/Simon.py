9f196d71f28c3ff258113998e29d86e8aa358b2f
Harrbit <2471691727@qq.com>
2024-10-09 11:18:45 -0700

    several problems solved

---
+++ com_sci_238/Simon_and_Grover/Simon.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4f5d1be30ff9f59e7cc785971a4d31dbcfc4eaef72c9f8ed3bda60b1cb4e2be5
+size 240
--- physics_245/hwk_1/prb_2.py
+++ physics_245/hwk_1/prb_2.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:642f51f77a70e24e2d247df7c27aed3f79ff2c300a74f042ddef4a6796ac0737
+oid sha256:292e6efece63b0785ef32d3a454f4a86d9b983b3f971fdec8b1f0d8e5367deb6
 size 1544
