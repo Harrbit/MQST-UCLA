17d596ff44a5ac783c080f7e503cc4d20d97bed0
Harrbit <2471691727@qq.com>
2024-10-09 17:25:25 -0700

    simon and grover done

--- com_sci_238/Simon_and_Grover/Simon.py
+++ com_sci_238/Simon_and_Grover/Simon.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f5d1be30ff9f59e7cc785971a4d31dbcfc4eaef72c9f8ed3bda60b1cb4e2be5
-size 240
+oid sha256:fce204663bc2ae875e6566022c6d16726cc20ebcdb22383f11063913e4622f88
+size 2963
