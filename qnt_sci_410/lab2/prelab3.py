af097c2352d4f6094afbc8659f40ac65c199ee78
Harrbit <2471691727@qq.com>
2024-11-03 23:35:06 -0800

    prelab P done

--- qnt_sci_410/lab2/prelab3.py
+++ qnt_sci_410/lab2/prelab3.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3dcb5c09917d07108b4a5c28345bfffac43c41b7d9d6704f8955493e35f9dce4
-size 2646
+oid sha256:151c154384d91ad5d50136a3017c80988f7082dbbbf13f7f8d193d5d7ebc98d7
+size 2661
