f2ee5457470ed9a750ff6ccd217603b867e48e17
Harrbit <2471691727@qq.com>
2024-11-13 02:53:37 -0800

    minor update

--- qnt_sci_410/lab3a/PhotonSim.py
+++ qnt_sci_410/lab3a/PhotonSim.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81319e35d367feed387190aa3eae8ce5b108165dc2e0bc04c0a092ec1943749e
-size 13678
+oid sha256:905ff4b7cd0c2d814c6cf537200cafd67de7a948f3809387d32b8f4dcc42879f
+size 13637
